6002ff6ace740a0d1f9d2f24c3ee9d7f1d9e3974
vbessuges <vincent.bessuges@webedia-group.com>
2018-04-17 08:29:12 +0200

    Prefix by vbe

--- app/src/main/java/fr/vbe/android/ui/test/activities/TestBottomNavigationActivity.kt
+++ app/src/main/java/fr/vbe/android/ui/test/activities/TestBottomNavigationActivity.kt
@@ -2,10 +2,10 @@ package fr.vbe.android.ui.test.activities
 
 import android.os.Bundle
 import android.widget.TextView
-import fr.vbe.android.ui.activities.BottomNavigationActivity
+import fr.vbe.android.ui.activities.VBEBottomNavigationActivity
 import fr.vbe.android.ui.test.R
 
-class TestBottomNavigationActivity : BottomNavigationActivity() {
+class TestBottomNavigationActivity : VBEBottomNavigationActivity() {
 
     override fun menuRes() = R.menu.test_bottom_navigation_activity
 
--- ui/src/main/java/fr/vbe/android/ui/activities/VBEBottomNavigationActivity.kt
+++ ui/src/main/java/fr/vbe/android/ui/activities/VBEBottomNavigationActivity.kt
@@ -5,13 +5,13 @@ import android.os.Bundle
 import android.support.annotation.MenuRes
 import android.support.v7.app.AppCompatActivity
 import fr.vbe.android.ui.R
-import fr.vbe.android.ui.databinding.LibUiActivityBottomNavigationBinding
+import fr.vbe.android.ui.databinding.VbeActivityBottomNavigationBinding
 
-abstract class BottomNavigationActivity : AppCompatActivity() {
+abstract class VBEBottomNavigationActivity : AppCompatActivity() {
 
-    private lateinit var binding: LibUiActivityBottomNavigationBinding
+    private lateinit var binding: VbeActivityBottomNavigationBinding
     val container by lazy {
-        binding.container
+        binding.vbeContainer
     }
 
     @MenuRes
@@ -19,7 +19,7 @@ abstract class BottomNavigationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.lib_ui__activity_bottom_navigation)
-        binding.navigation.inflateMenu(menuRes())
+        binding = DataBindingUtil.setContentView(this, R.layout.vbe_activity_bottom_navigation)
+        binding.vbeNavigation.inflateMenu(menuRes())
     }
 }
