b6b50dd032724441272b1af6a49e280fdf491db8
vbessuges <vincent.bessuges@webedia-group.com>
2018-04-26 09:12:57 +0200

    Add VBEDialog

--- app/src/main/java/fr/vbe/android/ui/test/HomeActivity.kt
+++ app/src/main/java/fr/vbe/android/ui/test/HomeActivity.kt
@@ -3,20 +3,28 @@ package fr.vbe.android.ui.test
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.os.Bundle
-import fr.vbe.android.base.BaseActivity
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import fr.vbe.android.base.text.VBELabel
+import fr.vbe.android.ui.dialog.VBEDialog
 import fr.vbe.android.ui.test.activities.TestBottomNavigationActivity
-import fr.vbe.android.ui.test.activities.TestFABActivity
 import kotlin.reflect.KClass
 
 /**
  * Created by Vincent on 4/15/2018.
  */
-class HomeActivity : BaseActivity() {
+class HomeActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        TestBottomNavigationActivity::class.create(this)
+    override fun onResume() {
+        super.onResume()
+
+        VBEDialog(this,
+                title = VBELabel(R.string.app_name),
+                message = VBELabel("Do you want to start TestBottomNavigationActivity?"),
+                positive = VBEDialog.ButtonConfig("Yes", { TestBottomNavigationActivity::class.create(this) }),
+                negative = VBEDialog.ButtonConfig("Nope", { Toast.makeText(this, "Well ok", Toast.LENGTH_SHORT).show() }),
+                neutral = VBEDialog.ButtonConfig("Hmmm", { Toast.makeText(this, "ok switzerland", Toast.LENGTH_SHORT).show() })
+        ).show()
     }
 }
 
